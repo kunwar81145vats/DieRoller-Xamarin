922672b71507020cf7bf54cdefbef98cd34dad30
kunwar81145vats <97743194+kunwar81145vats@users.noreply.github.com>
2022-08-02 13:22:38 -0400

    Commit: Die class implemented on Main page Details: 1. Input fields linked to code behind to update die object. 2. Condition implemented that current side up is less than total sides. 3. Roll method implemented and result displayed on Main page.

--- DieRoller/DieRoller/DieClass.cs
+++ DieRoller/DieRoller/DieClass.cs
@@ -2,11 +2,87 @@
 namespace DieRoller
 {
     public class DieClass
-    {
+    { 
+        private int totalSides { get; set; }
+        private string name { get; set; }
+        private int currentSide { get; set; }
 
+        //getter method for total sides
+        public int getTotalSides()
+        {
+            return totalSides;
+        }
+
+        //setter method for total sides
+        public void setTotalSides(int sides)
+        {
+            totalSides = sides;
+        }
+
+        //getter method for name
+        public string getName()
+        {
+            return name;
+        }
+
+
+        //setter method for name
+        public void setName(string value)
+        {
+            name = value;
+        }
+
+        //getter method for current side
+        public int getCurrentSide()
+        {
+            return currentSide;
+        }
+
+        //setter method for current side
+        public void setCurrentSide(int value)
+        {
+            currentSide = value;
+        }
+
+        //Default constructor creates 6 sided Die
         public DieClass()
         {
+            currentSide = 1;
+            name = "d6";
+            totalSides = 6;
+        }
 
+        //Constructor with 1 parameter to set only total sides
+        public DieClass(int sides)
+        {
+            currentSide = 1;
+            name = "d" + sides;
+            totalSides = sides;
+        }
+
+        //Constructor with 2 parameters to set total sides and name
+        public DieClass(int sides, string nameValue)
+        {
+            currentSide = 1;
+            name = nameValue;
+            totalSides = sides;
+        }
+
+        //Constructor with 3 parameters to set total sides, name and current side
+        public DieClass(int sides, string nameValue, int current)
+        {
+            currentSide = current;
+            name = nameValue;
+            totalSides = sides;
+        }
+
+        //Roll random side
+        public int rollDie()
+        {
+            var rnd = new Random();
+            var newNum = rnd.Next(1, totalSides);
+
+            return newNum;
         }
     }
 }
--- DieRoller/DieRoller/MainPage.xaml.cs
+++ DieRoller/DieRoller/MainPage.xaml.cs
@@ -10,14 +10,45 @@ namespace DieRoller
 {
     public partial class MainPage : ContentPage
     {
+        DieClass die = new DieClass();
+        public string ResultValue { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+            BindingContext = this;
         }
 
         void Button_Clicked(System.Object sender, System.EventArgs e)
         {
+            if(die.getCurrentSide() > die.getTotalSides())
+            {
+                 App.Current.MainPage.DisplayAlert("Alert", "Current side should be less than total number of sides.", "OK");
+            }
+            else
+            {
+                int result = die.rollDie();
+                ResultValue = result.ToString();
+                OnPropertyChanged(nameof(ResultValue));
+            }
+        }
 
+        void name_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            string name = ((Entry)sender).Text;
+            die.setName(name);
+        }
+
+        void sides_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            int sides = int.Parse(((Entry)sender).Text);
+            die.setTotalSides(sides);
+        }
+
+        void currentSide_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        {
+            int current = int.Parse(((Entry)sender).Text);
+            die.setCurrentSide(current);
         }
     }
 }
